ebd7a6de8696c2356f39a29a5eebb04c5b0dfd78
Thomas Chopitea <tomchop@gmail.com>
2023-03-29 17:30:06 +0000

    fastapi changes

--- dev/init.sh
+++ dev/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone git@github.com:yeti-platform/yeti.git
-pushd yeti && git checkout frontend && popd
+pushd yeti && git checkout fastapi && popd
 git clone git@github.com:yeti-platform/yeti-feeds-frontend.git
 
 docker-compose build api
