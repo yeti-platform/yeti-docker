27f354587d38801dc6a111e339318a286d764655
tomchop <tomchop@gmail.com>
2023-06-18 21:47:53 +0200

    new docker compose command

--- dev/init.sh
+++ dev/init.sh
@@ -6,5 +6,5 @@ pushd yeti && git checkout fastapi && popd
 git clone git@github.com:yeti-platform/yeti-feeds-frontend.git
 pushd yeti-feeds-frontend && git checkout fastapi && popd
 
-docker-compose build fastapi frontend
-docker-compose up -d
+docker compose build fastapi frontend
+docker compose up -d
