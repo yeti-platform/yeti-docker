4a9e7865030917b4510b4b8300413d62fc890b87
tomchop <tomchop@gmail.com>
2023-11-12 14:50:08 +0100

    Remove fastapi branching shenanigans

--- dev/init.ps1
+++ dev/init.ps1
@@ -1,15 +1,8 @@
 # Cloner le répertoire yeti
 git clone git@github.com:yeti-platform/yeti.git
-Set-Location -Path .\yeti
-git checkout fastapi
-Set-Location -Path ..
 
 # Cloner le répertoire yeti-feeds-frontend
 git clone git@github.com:yeti-platform/yeti-feeds-frontend.git
-Set-Location -Path .\yeti-feeds-frontend
-git checkout fastapi
-Set-Location -Path ..
 
 # Construire et démarrer les conteneurs Docker
-docker-compose build fastapi frontend
-docker-compose up -d
+docker-compose up
