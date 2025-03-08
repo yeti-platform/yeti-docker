c32fc0f4d7515b40446ee56c38883c72b485f786
tomchop <tomchop@gmail.com>
2025-03-08 17:44:28 +1100

    Add config file copy

--- dev/init.ps1
+++ dev/init.ps1
@@ -1,8 +1,12 @@
 # Cloner le répertoire yeti
 git clone https://github.com/yeti-platform/yeti.git
+# Copier le fichier de configuration
+Copy-Item -Path "yeti/yeti.conf.sample" -Destination "yeti/yeti.conf"
 
 # Cloner le répertoire yeti-feeds-frontend
 git clone https://github.com/yeti-platform/yeti-feeds-frontend.git
+
+
 
 # Construire et démarrer les conteneurs Docker
 docker-compose up
--- dev/init.sh
+++ dev/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/yeti-platform/yeti.git
+cp yeti/yeti.conf.sample yeti/yeti.conf
 
 git clone https://github.com/yeti-platform/yeti-feeds-frontend.git
 
