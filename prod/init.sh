086d8762024c60e782f32474c15274d1d9645a76
tomchop <tomchop@gmail.com>
2025-03-12 13:59:27 +1100

    Update init.sh script

--- prod/init.sh
+++ prod/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SECRET_KEY=$(openssl rand -hex 64)
-sed -i '' "s/^YETI_AUTH_SECRET_KEY=.*/YETI_AUTH_SECRET_KEY=$SECRET_KEY/" yeti/.env
+echo YETI_AUTH_SECRET_KEY=$SECRET_KEY >> .env
 docker compose up -d --wait
